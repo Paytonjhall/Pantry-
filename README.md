2c1a2b256f17a1a12d017293a41814f53a30dc64
Payton <paytonjhall@gmail.com>
2023-01-19 14:27:42 -0700

    added login page

---
+++ README.md
@@ -0,0 +1,4 @@
+TODO:
+1 - Add a recipe repo : where we pull existing recipes from, add new ones, etc.
+2 - pantry: We can add items we have in our pantry, list of items. I have a few ideas for this. 
+3 - work on gui
---
+++ src/Pantry/Stock.java
@@ -0,0 +1,6 @@
+package Pantry;
+
+public class Stock {
+  //This class will be what we use to keep track of all the items in the pantry
+  //We will be saving it out using json and loading back into it the same way.
+}
